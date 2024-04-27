d60acc8b1604f9b26f55e15703f7e13bf01e0635
djjaring <danjayjaring41@gmail.com>
2024-04-27 17:12:21 -0400

    added comment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! Hello Philippines");
+        // ask the user for name
+        Console.Write("What is your name?");
+        string first = Console.ReadLine();
+
+        Console.Write("what is your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($" Your name is {last},{first} {last}");
+        
+        
+        
+        
+        
     }
 }
