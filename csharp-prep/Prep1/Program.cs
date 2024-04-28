ee7e9003c897bdab28d782cb0f99bba6a8fdce52
djjaring <danjayjaring41@gmail.com>
2024-04-27 21:12:41 -0400

    added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // ask the user name for there name.
+        Console.Write ("what is your First name?");
+        string first= Console.ReadLine();
+        Console.Write("What is your last name?");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your last name is {last},{first} {last}");
+
+
+
+
     }
+
 }
