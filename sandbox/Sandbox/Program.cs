f8c131fd46965233f6695699d110d438be57c69d
djjaring <danjayjaring41@gmail.com>
2024-04-27 20:44:35 -0400

    prep 3 with stretch

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,43 +4,36 @@ using System.Drawing;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.Write("Enter your grade percentage: ");
-        double grade = double.Parse(Console.ReadLine());
-        string letter;
-        string sign = "";
+   {
+        string response;
 
-        // Determine the letter grade
-        if (grade >= 90)
-            letter = "A";
-        else if (grade >= 80)
-            letter = "B";
-        else if (grade >= 70)
-            letter = "C";
-        else if (grade >= 60)
-            letter = "D";
-        else
-            letter = "F";
+        do
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+            int guess;
+            int guessCount = 0;
 
-        // Determine the sign
-        int lastDigit = (int)grade % 10;
-        if (lastDigit >= 7 && letter != "A" && letter != "F")
-            sign = "+";
-        else if (lastDigit < 3 && letter != "A" && letter != "F")
-            sign = "-";
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                guessCount++;
 
-        Console.WriteLine("Your letter grade is: " + letter + sign);
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (guess != magicNumber);
 
-        // Determine if the user passed
-        if (grade >= 70)
-            Console.WriteLine("Congratulations! You passed the course.");
-        else
-            Console.WriteLine("You did not pass the course. Better luck next time!");
-    
-        
-        
-        
-        
-        
+            Console.WriteLine($"You guessed it! It took you {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            response = Console.ReadLine();
+        } while (response.ToLower() == "yes");
     }
 }
