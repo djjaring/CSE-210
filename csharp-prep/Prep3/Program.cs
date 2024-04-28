99c4ab55ef686d77424f891a437876ca251904b7
djjaring <danjayjaring41@gmail.com>
2024-04-27 21:21:36 -0400

    added stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,36 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+ {
+        string response;
+
+        do
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+            int guess;
+            int guessCount = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (guess != magicNumber);
+
+            Console.WriteLine($"You guessed it! It took you {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            response = Console.ReadLine();
+        } while (response.ToLower() == "yes");
     }
 }
