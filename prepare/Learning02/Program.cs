9a7180b4ee55f4c5d595eb692bb8714e4d605c62
djjaring <danjayjaring41@gmail.com>
2024-05-04 18:38:57 -0400

    job.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,14 @@
 using System;
 
-class Program
+public class Job
 {
-    static void Main(string[] args)
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
