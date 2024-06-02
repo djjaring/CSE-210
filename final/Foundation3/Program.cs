c6f0d438de3caecdd4344020a76428fc5059020f
djjaring <danjayjaring41@gmail.com>
2024-06-02 18:05:45 -0400

    commit

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,132 @@
 using System;
+using System.Collections.Generic;
+
+public class Address
+{
+    private string street;
+    private string city;
+    private string state;
+    private string zipCode;
+
+    public Address(string street, string city, string state, string zipCode)
+    {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public override string ToString()
+    {
+        return $"{street}, {city}, {state} {zipCode}";
+    }
+}
+
+public class Event
+{
+    private string title;
+    private string description;
+    private string date;
+    private string time;
+    private Address address;
+
+    public Event(string title, string description, string date, string time, Address address)
+    {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.address = address;
+    }
+
+    public virtual string GetStandardDetails()
+    {
+        return $"{title}\n{description}\nDate: {date}\nTime: {time}\nAddress: {address}";
+    }
+
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails();
+    }
+
+    public virtual string GetShortDescription()
+    {
+        return $"{GetType().Name}: {title} on {date}";
+    }
+}
+
+public class Lecture : Event
+{
+    private string speaker;
+    private int capacity;
+
+    public Lecture(string title, string description, string date, string time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        this.speaker = speaker;
+        this.capacity = capacity;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Lecture\nSpeaker: {speaker}\nCapacity: {capacity}";
+    }
+}
+
+public class Reception : Event
+{
+    private string rsvpEmail;
+
+    public Reception(string title, string description, string date, string time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        this.rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Reception\nRSVP Email: {rsvpEmail}";
+    }
+}
+
+public class OutdoorGathering : Event
+{
+    private string weatherForecast;
+
+    public OutdoorGathering(string title, string description, string date, string time, Address address, string weatherForecast)
+        : base(title, description, date, time, address)
+    {
+        this.weatherForecast = weatherForecast;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Outdoor Gathering\nWeather Forecast: {weatherForecast}";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address1 = new Address("123 Main St", "Anytown", "Anystate", "12345");
+        Address address2 = new Address("456 Elm St", "Othertown", "Otherstate", "67890");
+        Address address3 = new Address("789 Oak St", "Sometown", "Somestate", "11223");
+
+        Event lecture = new Lecture("The Future of FLYING CAR", "A lecture of Flying Car", "12/01/2024", "10:00 AM", address1, "John Doe", 100);
+        Event reception = new Reception("Company Gala", "Annual company celebration", "12/02/2024", "7:00 PM", address2, "rsvp@company.com");
+        Event outdoorGathering = new OutdoorGathering("Community Pickleball game", "A fun Pickleball game for the whole community", "12/03/2024", "1:00 PM", address3, "Sunny and warm");
+
+        List<Event> events = new List<Event> { lecture, reception, outdoorGathering };
+
+        foreach (Event ev in events)
+        {
+            Console.WriteLine(ev.GetStandardDetails());
+            Console.WriteLine();
+            Console.WriteLine(ev.GetFullDetails());
+            Console.WriteLine();
+            Console.WriteLine(ev.GetShortDescription());
+            Console.WriteLine();
+        }
     }
 }
